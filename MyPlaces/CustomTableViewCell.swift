6dbeaefd19915bb279db21f82e6a4c9fcad0c772
John Smith <app.mobile.368@gmail.com>
2020-07-19 00:02:44 -0700

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by NextUser on 7/18/20.
+//  Copyright © 2020 Swift&Xcode. All rights reserved.
+//
+
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
