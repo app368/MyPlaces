f73528edafa8f24662053c5648bed1665d062e0c
John Smith <app.mobile.368@gmail.com>
2020-09-01 12:42:59 -0700

    Prepare for Map

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by NextUser on 9/1/20.
+//  Copyright © 2020 Swift&Xcode. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+         
+    }
+    
+    @IBAction func closeVC() {
+        
+        dismiss(animated: true)
+    }
+    
+
+}
